ec82134b2eda3ffdb9b678e96654f820a78e8d9b
张龙涛 <319513897@outlook.com>
2020-09-14 15:57:00 +0800

    使用新的算法

--- xxd/main.cpp
+++ xxd/main.cpp
@@ -1,15 +1,27 @@
-/* Created by 31951 on 2020/8/19.
- * ASCII范围 http://ascii.911cha.com/
- */
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <memory>
+#include <sstream>
+#include <cstring>
 
 using namespace std;
 
+/*
+00000000: 8fff 7cf9 0100 0000 0100 0000 1000 0000  ..|.............
+00000010: 8dff 7cf9 0f00 0000 5345 204c 696e 7578  ..|.....SE Linux
+00000020: 204d 6f64 756c 6502 0000 0014 0000 0001   Module.........
+00000030: 0000 0008 0000 0000 0000 0008 0000 006d  ...............m
+00000040: 792d 6e67 696e 7803 0000 0031 2e30 4000  y-nginx....1.0@.
+00000050: 0000 0000 0000 0000 0000 0000 0000 0000  ...............
+
+一行一共是 8*4 个16进制数, 一共是 4*8*4 = 128 位
+字节数一共是 128/8 = 16 个字节
+ 在中间部分中，每 2 个字节组成一组，一共8组
+
+ */
+
 const size_t LENGTH = 16; // 每行输出 16 个字节
-const size_t SLICE = 2; // 每两个字节分到一组
 const unsigned char FALLBACK = '.'; // 若字符不是 ASCII 字符，输出此字符
 
 const char *helper = \
@@ -21,15 +33,11 @@ xxd -h
 xxd 1.txt
 cat 1.txt | xxd)";
 
-inline const char *left();
+void left();
 
-inline string cent(const char *begin, const char *end);
+void center(const char *);
 
-inline string right(const char *begin, const char *end);
-
-inline void output(const char *data, size_t len);
-
-inline void out(string &data);
+void right(const char *);
 
 int main(int argc, char **argv) {
     string filename;
@@ -41,78 +49,51 @@ int main(int argc, char **argv) {
         }
         filename = argv[i];
     }
-    string buf;
-    string data;
-    if (filename.empty()) {
-        while (getline(cin, buf)) {
-            data.append(buf);
-            data.append("\n");
-        };
-        out(data);
-    } else {
-        fstream fs(filename, ios::in | ios::out | ios::binary);
-        if (!fs.is_open()) return 0;
-        while (getline(fs, buf)) {
-            data.append(buf);
-            data.append("\n");
-        };
-        out(data);
-    }
-}
-
-
-const char *left() {
-    static unsigned *number = nullptr; // 用来保存状态
-    char *buf = new char[LENGTH / 2 + 1];
-    if (!number) {
-        number = new unsigned(0);
-        sprintf_s(buf, LENGTH / 2 + 1, "%08x", *number);
-        return buf;
-    }
-    *number += LENGTH;
-    sprintf_s(buf, LENGTH / 2 + 1, "%08x", *number);
-    return buf;
-}
-
-string cent(const char *begin, const char *end) {
-    static char buf[LENGTH * 2];
-    for (int i = 0; i < LENGTH && (begin + i != end); ++i) {
-        sprintf_s((buf + i * 2), SLICE + 1, "%x", *(begin + i));
-    }
-    static string str;
-    str.clear();
-    for (int i = 0; i < LENGTH * 2; ++i) {
-        str += (buf[i]);
-        if (!((i + 1) % (SLICE * 2))) str.append(" ");
-    }
-    return str;
-}
-
-string right(const char *begin, const char *end) {
-    static string str;
-    str.clear();
-    for (int i = 0; i < LENGTH && (begin + i != end); ++i) {
-        str += begin[i];
-    }
-    return str;
-}
-
-void output(const char *data, size_t len) {
-    string str(data);
-    cout << left() << ": ";
-    cout << cent(data, data + min(LENGTH, len)) << "  ";
-    cout << right(data, data + min(LENGTH, len)) << endl;
-}
-
-void out(string &data) {
-    for (size_t i = 0; i < data.length(); ++i) { // 格式化字符串
-        if (data[i] < 32 || data[i] > 126) data[i] = FALLBACK;
-    }
-    for (size_t i = 0; i < data.length(); i += LENGTH) {
-        if (i + LENGTH >= data.length()) {
-            output(data.c_str() + i, data.length() - i);
-            continue;
+    // 获取输入流
+    istream *is = nullptr;
+    if (filename.empty()) is = &cin;
+    else is = new ifstream(filename, ios::in | ios::binary);
+    // 打开异常
+    is->exceptions(is->exceptions() | ios::failbit | ios::badbit);
+    // 现在一行一行地进行解析
+    char buf[LENGTH];
+    try {
+        while (is->get(buf, LENGTH)) {
+            left();
+            center(buf);
+            right(buf);
         }
-        output(data.c_str() + i, LENGTH);
+    } catch (exception &e) {
+        cout << endl << "error:" << e.what() << endl;
     }
+    auto hand = dynamic_cast<ifstream *>(is);
+    if (hand) hand->close();
+    return 0;
+}
+
+void left() {
+    static size_t start = 0;
+    printf("%08zx: ", start);
+    start += LENGTH;
+}
+
+void center(const char *str) {
+    uint16_t buf = 0;;
+    uint8_t *buf_mul = (uint8_t *) &buf;
+    // 一次输出两个字节，一共16个字节，需要输出8次
+    for (int i = 0; i < 16; i += 2) {
+        buf_mul[0] = str[i];
+        buf_mul[1] = str[i + 1];
+        printf("%04x ", buf);
+    }
+}
+
+void right(const char *str) {
+    static char buf[LENGTH + 1];
+    buf[LENGTH] = '\0';
+    memcpy_s(buf, LENGTH + 1, str, LENGTH);
+    for (int i = 0; i < LENGTH; ++i)
+        if (buf[i] > 126 || str[i] < 32)
+            buf[i] = FALLBACK;
+    printf("%s\n", buf);
 }
