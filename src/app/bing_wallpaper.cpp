67d9003e3e49bac7145215aa152d6cef2e3d5d90
张龙涛 <319513897@outlook.com>
2021-04-24 17:26:08 +0800

    project restruct

--- src/app/bing_wallpaper.cpp
+++ src/app/bing_wallpaper.cpp
@@ -1,18 +1,85 @@
-#include <libintl.h>
-#include <cxxopts.hpp>
-#include <fmt/format.h>
-#include <iostream>
+/***
+ * a program to set bing wallpaper
+ *
+ */
+
+// run behind
+#pragma comment(linker, "/subsystem:windows /entry:mainCRTStartup")
 #include <Config.h>
+#include <cpr/cpr.h>
+#include <fmt/format.h>
+#include <libintl.h>
+#include <spdlog/spdlog.h>
+#include <windows.h>
+
+#include <chrono>
+#include <cxxopts.hpp>
+#include <filesystem>
+#include <iomanip>
+#include <iostream>
 
 #define _(str) (gettext(str))
-//#define  _(str) (str)
 
+constexpr char BASE_URL[] = "https://cn.bing.com";
+constexpr char API[]      = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN";
 
-int main(int argc, char** argv){
+using namespace std;
+using namespace std::chrono;
+using namespace std::filesystem;
+
+/***
+ * Get a filesystem path wallpaper showld be store
+ */
+string getFileName() {
+    auto aTimet = system_clock::to_time_t(system_clock::now());
+
+    auto atime  = new tm();
+    localtime_s(atime, &aTimet);
+
+    ostringstream os;
+    os << put_time(atime, R"(%Y-%m-%d)");
+    string filename = "D:/Pictures/bingWallpaper/" + os.rdbuf()->str() + ".png";
+    return std::move(filename);
+}
+
+string getUrl() {
+    cpr::Response r = cpr::Get(cpr::Url { API });
+    if(r.status_code != 200) { spdlog::critical(_("get wallpaper url from {0} failed. terminate"), r.url.c_str()); }
+    // 对 json 进行解析
+    spdlog::debug(_("the JSON got from server is {0}"), r.text);
+    auto   left  = r.text.find("url") + 6;
+    auto   right = r.text.find('\"', left);
+    string url   = BASE_URL;
+    for_each(r.text.begin() + left, r.text.begin() + right, [&](auto el) {
+        url += el;
+    });
+    spdlog::debug("the URL of wallpaper is {0}", url);
+    return std::move(url);
+}
+
+int main(int argc, char** argv) {
+    // i18n
     setlocale(LC_ALL, "");
     bindtextdomain(Config::PACKAGE, Config::LOCALEDIR);
     textdomain(Config::PACKAGE);
+    // commandline parse
     cxxopts::Options options(_("bing_wallpaper"), _("Download bing wallpaper and set it"));
-    std::cout<<options.help();
+    //
+    spdlog::set_level(spdlog::level::debug);
+    auto filename = getFileName();
+    if(!filesystem::exists(filename)) {
+        spdlog::info(_("wallpaper download ready to: {0}"), filename);
+        // 下载图片
+        cpr::Response r = cpr::Get(cpr::Url { getUrl() });
+        if(r.status_code != 200) { spdlog::critical(_("download wallpaper by {0} failed, terminate"), r.url.c_str()); }
+        ofstream ofs(filename, ios::binary | ios::trunc);
+        ofs.write(r.text.c_str(), r.text.length());
+        ofs.close();
+    } else {
+        spdlog::info(_("wallpaper fill already exists, skip. {0}"), filename);
+        return 0;
+    }
+    // wallpaper must be downloaded
+    SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, (PVOID)filename.c_str(), SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
     return 0;
 }
